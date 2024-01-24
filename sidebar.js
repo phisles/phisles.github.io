1743d9b2f42610f8bdce5d9b06bf4607e2ee7ada
phisles <156632514+phisles@users.noreply.github.com>
2024-01-24 06:54:28 -0800

    sidebar

--- sidebar.js
+++ sidebar.js
@@ -1,16 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Create a new div element for the sidebar
     var sidebarDiv = document.createElement("div");
     sidebarDiv.className = "sidebar";
-  
-    // Add the sidebar content
     sidebarDiv.innerHTML = `
-      <a href="https://pisles-misc.carrd.co/" target="_blank">Supplementary Information</a>
-      <a href="https://pisles-cert.carrd.co/" target="_blank">View My Certifications</a>
-      <a href="https://philipisles.com/gpt-projects.html" target="_blank">Check Out My GPT Projects</a>
+        <a href="https://pisles-misc.carrd.co/" target="_blank">Supplementary Information</a>
+        <a href="https://pisles-cert.carrd.co/" target="_blank">View My Certifications</a>
+        <a href="https://philipisles.com/gpt-projects.html" target="_blank">Check Out My GPT Projects</a>
     `;
-  
-    // Append the sidebar to the body
     document.body.prepend(sidebarDiv);
-  });
-  
+});
