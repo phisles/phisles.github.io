b3892a21c77c0ef49ebdbcaf8a0f5a207d711a6e
Philip Isles <156632514+phisles@users.noreply.github.com>
2024-07-18 10:40:44 -0700

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
         <a href="https://philipisles.com/gpt-projects.html" target="_blank">Check Out My GPT Projects</a>
         <a href="https://philipisles.com/nasa.html" target="_blank">NASA API</a>
         <a href="https://github.com/phisles?tab=repositories" target="_blank">Git Projects</a>
+        <a href="https://philipisles.com/sentiment.html" target="_blank">Sentiment Analysis</a>
         <a href="https://philipisles.com/index.html" target="_blank">Home</a>
     `;
     document.body.prepend(sidebarDiv);
