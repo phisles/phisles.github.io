6b8e87d6461144579f5bfd29bcc3fd4a4b1e8b79
Philip Isles <156632514+phisles@users.noreply.github.com>
2025-09-27 14:39:08 -0700

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,19 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var sidebarDiv = document.createElement("div");
-    sidebarDiv.className = "sidebar";
-    sidebarDiv.innerHTML = `
-        <a href="https://pisles-misc.carrd.co/" target="_blank">Supplementary Information</a>
-        <a href="https://pisles-cert.carrd.co/" target="_blank">View My Certifications</a>
-        <a href="https://philipisles.com/gpt-projects.html" target="_blank">Check Out My GPT Projects</a>
-        <a href="https://philipisles.com/nasa.html" target="_blank">NASA API</a>
-        <a href="https://github.com/phisles?tab=repositories" target="_blank">Git Projects</a>
-        <a href="https://www.linkedin.com/in/philipisles/" target="_blank">Linkedin Page</a>
-        <a href="https://philipisles.com/sentiment.html" target="_blank">Network Isolated Sentiment Analysis</a>
-        <a href="https://philipisles.com/local-vid-desc.html" target="_blank">Network Isolated Video Description</a>
-        <a href="https://philipisles.com/mistral-chunk.html" target="_blank">Network Isolated Chunked Transcript Summarization</a>
-        <a href="http://159.223.201.65/" target="_blank">Crypto Algo Tracker</a>
-        <a href="https://hedge-2.streamlit.app/" target="_blank">Stock Algo Tracker</a>
-        <a href="https://philipisles.com/index.html" target="_blank">Home</a>
-    `;
-    document.body.prepend(sidebarDiv);
+    var sidebarDiv = document.createElement("div");
+    sidebarDiv.className = "sidebar";
+    sidebarDiv.innerHTML = `
+        <a href="https://pisles-misc.carrd.co/" target="_blank">Supplementary Information</a>
+        <a href="https://pisles-cert.carrd.co/" target="_blank">View My Certifications</a>
+        <a href="https://philipisles.com/gpt-projects.html" target="_blank">Check Out My GPT Projects</a>
+        <a href="https://philipisles.com/nasa.html" target="_blank">NASA API</a>
+        <a href="https://github.com/phisles?tab=repositories" target="_blank">Git Projects</a>
+        <a href="https://www.linkedin.com/in/philipisles/" target="_blank">Linkedin Page</a>
+        <a href="https://philipisles.com/sentiment.html" target="_blank">Network Isolated Sentiment Analysis</a>
+        <a href="https://philipisles.com/local-vid-desc.html" target="_blank">Network Isolated Video Description</a>
+        <a href="https://philipisles.com/mistral-chunk.html" target="_blank">Network Isolated Chunked Transcript Summarization</a>
+        <a href="http://159.223.201.65/" target="_blank">Crypto Algo Tracker</a>
+        <a href="https://hedge-2.streamlit.app/" target="_blank">Stock Algo Tracker</a>
+        <a href="https://philipisles.com/index.html" target="_blank">Home</a>
+        <a href="https://docs.google.com/document/d/1g8YsAiftqTIcP5_qqBJBMYGiBxlv2EnNkbJpZGoCknw/edit?tab=t.0" target="_blank">Drink Menu</a>
+    `;
+    document.body.prepend(sidebarDiv);
 });
